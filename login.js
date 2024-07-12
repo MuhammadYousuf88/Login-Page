de395a02f0155a2cd044e27a467eb93ee159e3a5
Muhammad Yousuf <yousuf301c@gmail.com>
2024-07-11 23:51:45 -0700

    Initial commit

---
+++ login.js
@@ -0,0 +1,22 @@
+
+function input(){
+    let input1=document.getElementById("inp1").value;
+    let password=document.getElementById("password").value;
+    let input3=document.getElementById("inp3").value;
+    let eyeicon=document.getElementById("eyeicon");
+
+  
+
+    document.write("Your Name is "+ input1 + "<br />")
+    document.write("You Have Entered the Password "+ password + "<br />")
+    document.write("Your Email Address "+ input3 + "<br />")
+}
+
+eyeicon.onclick = function(){
+        if(password.type == "password"){
+            password.type= "text";
+        }
+        else{
+            password.type="password";
+        }
+    }
